d57f05e1ddc063abc8e040fb826db227bddbcaf8
Martin Holmberg <emartinholmberg@gmail.com>
2023-03-31 10:33:23 +0200

    Add project files.

---
+++ Tic-Tac-Toe/WinInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Tic_Tac_Toe
+{
+    public class WinInfo
+    {
+        public WinType Type { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Tic-Tac-Toe/WinType.cs
@@ -0,0 +1,7 @@
+﻿namespace Tic_Tac_Toe
+{
+    public enum WinType
+    {
+        Row, Column, MainDiagonal, AntiDiagonal
+    }
+}
